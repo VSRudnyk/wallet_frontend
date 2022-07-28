96602eda8301daa26c5e80d98acf0082af040031
VSRudnyk <v.rudnyk@gmail.com>
2022-07-28 13:57:06 +0300

    added color's variables

--- src/stylesheet/index.styled.js
+++ src/stylesheet/index.styled.js
@@ -47,4 +47,29 @@ export const GlobalStyle = createGlobalStyle`
   svg {
     fill: inherit;
   }
+
+  :root {
+    --black: #000000;
+    --gray: #bdbdbd;
+    --light-gray: #E0E0E0;
+    --dark-grey: #A6A6A6;
+    --backdrop: rgba(0, 0, 0, 0.25);
+    --diagram-shadow: rgba(255, 255, 255, 0.20);
+    --background-light-grey:rgba(255, 255, 255, 0.4);
+    --background-gray: #E5E5E5;
+    --white: #ffffff;
+    --green: #24CCA7;
+    --active-blue: #4A56E2;
+    --blue:#6E78E8;
+    --pink: #FF6596;
+    --vector-color:#E7E5F2;
+    --add-transaction-menu-color:rgba(255, 255, 255, 0.7);
+
+    --baseFont: 'Circe', sans-serif;
+    --secondaryFont: 'Poppins', sans-serif;
+
+    --regular: 400;
+    --bold: 700;
+
+  }
 `;
