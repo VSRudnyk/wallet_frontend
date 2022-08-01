2ddf00e2a9936a48a5df569fc9a31240d6a4b4b6
IrynaSam <52596926+msamsonenko@users.noreply.github.com>
2022-08-01 15:31:02 +0300

    languageSwitcher set up
    nested LanguageSwitcher component into Header component, add logic to lang switch, imported svg

--- src/components/LanguageSwitcher/LanguageSwitcher.styled.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.styled.js
@@ -1,14 +1,55 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  background-color: var(--active-blue);
-  color: var(--white);
+  display: flex;
+  background-color: transparent;
+
   border: none;
-  padding: 10px;
-  border-radius: 10px;
-  margin-bottom: 20px;
+
+  margin-right: 10px;
+`;
+export const BtnWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const DropDown = styled.div`
+  position: absolute;
+  top: 100%;
+  left: -75px;
+  width: max-content;
+  z-index: 2;
+`;
+export const List = styled.ul`
+  width: inherit;
+  border-radius: 20px;
+  overflow: hidden;
+
+  border: 1px solid rgba(0, 0, 0, 0.04);
+  box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14);
+`;
+export const ListItem = styled.li`
+  width: 100%;
+  &:not(:last-child) {
+    border-bottom: 1px solid;
+    border-bottom-color: var(--transaction-underline-color);
+  }
+`;
+export const LangBtn = styled.button`
+  width: 100%;
+  background-color: var(--white);
+
+  border: none;
+
+  padding: 12px 20px 8px 20px;
+
   font-family: var(--baseFont);
   font-weight: var(--bold);
   font-size: 18px;
   line-height: 27px;
+  &:hover {
+    background-color: var(--blue);
+    color: var(--white);
+    cursor: pointer;
+  }
 `;
--- src/redux/authOperation.jsx
+++ src/redux/authOperation.jsx
@@ -6,7 +6,7 @@ export const authOperation = createApi({
     baseUrl: 'https://wallet-backend-1.herokuapp.com/api',
     prepareHeaders: (headers, { getState }) => {
       const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MmU1OTYzNWI3ODZkYzI3NDA5OTk3YTMiLCJzaWQiOiI2MmU3OTEzMjg0Mjk3ODVlNGU2MWRkYzkiLCJpYXQiOjE2NTkzNDMxNTQsImV4cCI6MTY1OTM0Njc1NH0.oyUjcQkSluBhKD579OtZHwLNphizBGS0OJ2Qhh5CVgc';
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI2MmU1OTYzNWI3ODZkYzI3NDA5OTk3YTMiLCJzaWQiOiI2MmU3YzBiOTg0Mjk3ODVlNGU2MWRlMWIiLCJpYXQiOjE2NTkzNTUzMjEsImV4cCI6MTY1OTM1ODkyMX0.Olrrhp0QIqP1y3X6q1D5g7FN5a_W70aFzetFGufhDuU';
 
       headers.set('authorization', `Bearer ${token}`);
 
