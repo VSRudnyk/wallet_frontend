b10289b468036631f802213f669f515168348843
VSRudnyk <v.rudnyk@gmail.com>
2022-08-02 15:15:59 +0300

    Changed loader's size

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import { LoaderContainer } from './Loader.styled';
 export const Loader = () => {
   return (
     <LoaderContainer>
-      <HashLoader color="#1f38dd" />
+      <HashLoader color="#1f38dd" size={150} />
     </LoaderContainer>
   );
 };
