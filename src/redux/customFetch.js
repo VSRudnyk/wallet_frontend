353d4acf0f468d2e9a25759242e3a117cf4c35dd
Maxim <m.steblyna@omisoft.net>
2022-08-06 09:46:27 +0300

    deleted cookie logics

--- src/redux/customFetch.js
+++ src/redux/customFetch.js
@@ -1,8 +1,11 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { logOut, setCredentials } from './authSlice';
+import { Mutex } from 'async-mutex';
+
+const mutex = new Mutex();
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://wallet-backend-1.herokuapp.com/api/',
+  baseUrl: 'http://localhost:3000/api/',
   prepareHeaders: (headers, { getState }) => {
     const accessToken = getState().auth.accessToken;
     if (accessToken) {
@@ -12,32 +15,49 @@ const baseQuery = fetchBaseQuery({
   },
 });
 export const baseQueryWithReauth = async (args, api, extraOptions) => {
+  await mutex.waitForUnlock();
   let result = await baseQuery(args, api, extraOptions);
-  if (result?.error?.status === 401) {
-    const currentCredential = api.getState().auth;
-    const refreshResult = await baseQuery(
-      {
-        url: 'auth/refresh',
-        method: 'POST',
-        body: {
-          sid: `${currentCredential.sid}`,
-        },
-        credentials: 'include',
-      },
-      api,
-      extraOptions,
-    );
-    if (refreshResult.data) {
-      api.dispatch(
-        setCredentials({
-          ...refreshResult.data.data,
-          user: currentCredential.user,
-          isLoggedIn: currentCredential.isLoggedIn,
-        }),
-      );
-      result = await baseQuery(args, api, extraOptions);
+  const currentCredential = api.getState().auth;
+  api.dispatch(
+    setCredentials({
+      ...currentCredential,
+      accessToken: currentCredential.refreshToken,
+    }),
+  );
+  if (result.error && result.error.status === 401) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+      try {
+        const refreshResult = await baseQuery(
+          {
+            url: 'auth/refresh',
+            method: 'POST',
+            body: {
+              sid: `${currentCredential.sid}`,
+            },
+            credentials: 'include',
+          },
+          api,
+          extraOptions,
+        );
+        if (refreshResult.data) {
+          api.dispatch(
+            setCredentials({
+              ...refreshResult.data.data,
+              user: currentCredential.user,
+              isLoggedIn: currentCredential.isLoggedIn,
+            }),
+          );
+          result = await baseQuery(args, api, extraOptions);
+        } else {
+          api.dispatch(logOut);
+        }
+      } finally {
+        release();
+      }
     } else {
-      api.dispatch(logOut);
+      await mutex.waitForUnlock();
+      result = await baseQuery(args, api, extraOptions);
     }
   }
   return result;
