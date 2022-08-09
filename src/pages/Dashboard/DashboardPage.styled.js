b717356ada5fdb01532d5f828c28de247ea6b1af
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-08-10 00:55:16 +0300

    fix separator

--- src/pages/Dashboard/DashboardPage.styled.js
+++ src/pages/Dashboard/DashboardPage.styled.js
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
-// import BackDes from '../../images/BackgrDes-min.png';
-// import BackTab from '../../images/BackgrTablet-min.png';
-
 export const Dashboard = styled.main`
   height: 100%;
   padding-top: 75px;
@@ -41,15 +38,14 @@ export const DashboardSecondSectionWrapper = styled.div`
   position: relative;
   margin-left: auto;
   margin-right: auto;
-  // height: 70vh;
 `;
 export const DashboardSeparator = styled.div`
   @media screen and (min-width: 1280px) {
-    // height: 100%;
     margin-left: 69px;
     margin-right: 89px;
     margin-top: -47px;
-    margin-bottom: -103px;
+    margin-bottom: ${({ pathname }) =>
+      pathname === '/wallet_frontend/home' ? '-83px' : '-103px'};
     border: 1px solid var(--vector-color);
     box-shadow: -1px 0px 0px rgba(0, 0, 0, 0.05),
       1px 0px 0px rgba(255, 255, 255, 0.6);
